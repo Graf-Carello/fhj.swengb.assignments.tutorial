9922f51c21545d8b6da267f1b21fd55968f02392
Graf-Carello <felix.graf@edu.fh-joanneum.at>
2015-10-30 08:15:22 +0100

    Name change to fgraf

--- src/main/scala/fhj/swengb/assignments/tutorial/fgraf/TutorialAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tutorial/fgraf/TutorialAssignment.scala
@@ -1,4 +1,4 @@
-package fhj.swengb.assignments.tutorial.GrafCarello
+package fhj.swengb.assignments.tutorial.fgraf
 
 object TutorialAssignment {
   def hello() = "Hello world!"
--- src/test/scala/fhj/swengb/assignments/tutorial/fgraf/TutorialAssignmentTest.scala
+++ src/test/scala/fhj/swengb/assignments/tutorial/fgraf/TutorialAssignmentTest.scala
@@ -1,4 +1,4 @@
-package fhj.swengb.assignments.tutorial.GrafCarello
+package fhj.swengb.assignments.tutorial.fgraf
 
 import org.junit.Assert._
 import org.junit.Test
